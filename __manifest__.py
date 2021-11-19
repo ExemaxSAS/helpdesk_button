dc8c624c78993234350e61375772898a824e7f40
Ignacio Buioli <ibuioli@gmail.com>
2021-11-19 14:06:16 -0300

    doc: update manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,35 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "helpdesk_button",
+    'name': "Helpdesk Button",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Simple Button for report a Ticket on other Odoo Helpdesk """,
 
     'description': """
-        Long description of module's purpose
+        Simple Button for report a Ticket on other Odoo Helpdesk 
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Exemax",
+    'website': "https://www.exemax.com.ar",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'demo': [],
 }
